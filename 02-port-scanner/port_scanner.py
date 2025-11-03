40c74c8003558a99fb5a711832b5c357e919839d
elnathan <elnathannigussie@gmail.com>
2025-11-03 23:14:17 +0300

    altered port_scanner script

---
+++ 02-port-scanner/port_scanner.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+host = sys.argv[1] if len(sys.argv) >1 else "127.0.0.1"
+port=int(sys.arg[2]) if len(sys.argv)>2 else 22
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+#AF_INET = IPV4
+#SOCK_STREAM = TCP
+s.settimeout(1.0) #to stop soon
+try:
+    s.connect((host, port)) #attempt tcp connect
+    print(f"{host}:{port} is OPEN")
+except (socket.timeout, ConnectionRefusedError):
+    print(f"{host}:{port} is CLOSED or FILTERED")
+except Exception as e:
+    print("Error:", e)
+finally:
+    s.close()
