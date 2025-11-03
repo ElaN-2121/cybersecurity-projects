cc3c9f7144cc672da3db383617e7c67f39482cc0
elnathan <elnathannigussie@gmail.com>
2025-11-03 23:22:33 +0300

    Edited a typo

--- 02-port-scanner/port_scanner.py
+++ 02-port-scanner/port_scanner.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 host = sys.argv[1] if len(sys.argv) >1 else "127.0.0.1"
-port=int(sys.arg[2]) if len(sys.argv)>2 else 22
+port=int(sys.argv[2]) if len(sys.argv)>2 else 22
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 #AF_INET = IPV4
